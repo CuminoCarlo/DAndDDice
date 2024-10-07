32f52f95743234d88cd424fdbecd4ae1f14f357a
Carlo Cumino <ccumino89@gmail.com>
2024-10-07 12:33:46 +0200

    initial check in

---
+++ src/Dice.java
@@ -0,0 +1,48 @@
+import javax.xml.transform.Result;
+import java.util.Random;
+
+public class Dice {
+    Random random = new Random();
+    private int facce;
+    private int result;
+
+    public Dice(int s){
+        facce=s;
+    }
+
+    public void setFacce(int facce) {
+        this.facce = facce;
+    }
+
+    public int getFacce() {
+        return facce;
+    }
+
+    public void setResult(int result){
+        this.result=result;
+    }
+
+    public int getResult(int facce){
+        return result = 1+random.nextInt(facce);
+    }
+
+    public void D20Messagge(int facce, int result){
+        String message;
+        if (facce == 20) {
+            switch (result) {
+                case 1:
+                    message = result+"! Fallimento critico";
+                    break;
+                case 20:
+                    message = result+"! Complimenti! 20 naturale!";
+                    break;
+                default:
+                    message = "Your value is "+ String.valueOf(result);
+                    break;
+
+                }
+            System.out.println(message);
+        }
+
+    }
+}
