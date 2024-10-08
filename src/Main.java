ac1f9d4d9c26850928743ff0469c357954034c30
Carlo Cumino <ccumino89@gmail.com>
2024-10-08 08:57:13 +0200

    add instance of new class

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,15 @@ public class Main {
     public static void main(String[] args) {
         DTwenty twenty = new DTwenty();
         int r=twenty.Roll();
-        twenty.Messagge(twenty.getResult(r));
+        twenty.Message(twenty.getResult(r));
+
+        DFour dfour = new DFour();
+        int re= dfour.Roll();
+        dfour.Message(dfour.getResult(re));
 
         DEigth d8 = new DEigth();
         int s= d8.Roll();
-        d8.Messagge(d8.getResult(s));
+        d8.Message(d8.getResult(s));
 
     }
 
