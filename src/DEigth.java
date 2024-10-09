011b379e2d874c58ea25694b56a0e2aa291186b2
Carlo Cumino <ccumino89@gmail.com>
2024-10-09 16:11:34 +0200

    improve code

--- src/DEigth.java
+++ src/DEigth.java
@@ -2,7 +2,7 @@ public class DEigth extends Dice{
     private boolean isMasterWeapon;
     public DEigth(boolean isMasterWeapon){
         super(8); // Passa il numero di facce direttamente al costruttore della superclasse
-        this.isMasterWeapon=isMasterWeapon;
+        this.isMasterWeapon=isMasterWeapon; //Per determinare la superiorità del battle master
     }
 
     // Non è necessario sovrascrivere getFacce() e getResult() se non aggiungi funzionalità extra
